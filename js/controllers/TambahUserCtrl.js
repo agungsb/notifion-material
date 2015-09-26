ce09f25183c0afbb9f4de693a41c5e32fdd496a6
firdausibnu <firdausibnu@hotmail.com>
2015-09-26 22:49:14 +0700

    UPDATES TERUS

--- js/controllers/TambahUserCtrl.js
+++ js/controllers/TambahUserCtrl.js
@@ -1,80 +1,87 @@
 'use strict';
 
 function TambahUserCtrl($scope, $http, $state, $mdToast) {
+    
+    $scope.keepString = function(String) {
+        return String;
+    };
+    
     $http.get("http://localhost/notifion-api/users2").success(function(feedback) {
-         console.log(feedback);
-            if (feedback.result.length == 0) {
-                $scope.tableIsEmpty = true;
-            } else {
-                $scope.tableIsEmpty = false;
-            }
-            $scope.users = feedback.result;
-            $scope.tableReady = true;
-            
+        console.log(feedback);
+        if (feedback.result.length == 0) {
+            $scope.tableIsEmpty = true;
+        } else {
+            $scope.tableIsEmpty = false;
+        }
+        $scope.users = feedback.result;
+        $scope.tableReady = true;
+
     }).error(function(error) {
         console.log(error);
     });
-    
+
 //    get Instansi
     $http.get("http://localhost/notifion-api/instansi").success(function(feedback) {
-         console.log(feedback);
-            if (feedback.result_Instansi.length == 0) {
-                $scope.tableIsEmpty = true;
-            } else {
-                $scope.tableIsEmpty = false;
-            }
-            $scope.instansis = feedback.result_Instansi;
-            $scope.tableReady = true;
-            
+        console.log(feedback);
+        if (feedback.result_Instansi.length == 0) {
+            $scope.tableIsEmpty = true;
+        } else {
+            $scope.tableIsEmpty = false;
+        }
+        $scope.instansis = feedback.result_Instansi;
+        $scope.tableReady = true;
+
     }).error(function(error) {
         console.log(error);
     });
-    
+
     //    get kode hal
     $http.get("http://localhost/notifion-api/kodeHals").success(function(feedback) {
-         console.log(feedback);
-            if (feedback.result.length == 0) {
-                $scope.tableIsEmpty = true;
-            } else {
-                $scope.tableIsEmpty = false;
-            }
-            $scope.kodehals = feedback.result;
-            $scope.tableReady = true;
-            
+        console.log(feedback);
+        if (feedback.result.length == 0) {
+            $scope.tableIsEmpty = true;
+        } else {
+            $scope.tableIsEmpty = false;
+        }
+        $scope.kodehals = feedback.result;
+        $scope.tableReady = true;
+
     }).error(function(error) {
         console.log(error);
     });
-    
+
     //    get kode unit
     $http.get("http://localhost/notifion-api/kodeUnits").success(function(feedback) {
-         console.log(feedback);
-            if (feedback.result.length == 0) {
-                $scope.tableIsEmpty = true;
-            } else {
-                $scope.tableIsEmpty = false;
-            }
-            $scope.kodeunits = feedback.result;
-            $scope.tableReady = true;
-            
+        console.log(feedback);
+        if (feedback.result.length == 0) {
+            $scope.tableIsEmpty = true;
+        } else {
+            $scope.tableIsEmpty = false;
+        }
+        $scope.kodeunits = feedback.result;
+        $scope.tableReady = true;
+
     }).error(function(error) {
         console.log(error);
     });
-    
+
     /* Institusi */
     $http.get('http://localhost/notifion-api/institusi').success(function(feedback) {
         console.log(feedback);
-            if (feedback.result.length == 0) {
-                $scope.tableIsEmpty = true;
-            } else {
-                $scope.tableIsEmpty = false;
-            }
-            $scope.institusis = feedback.result;
-            $scope.tableReady = true;
+        if (feedback.result.length == 0) {
+            $scope.tableIsEmpty = true;
+        } else {
+            $scope.tableIsEmpty = false;
+        }
+        $scope.institusis = feedback.result;
+        $scope.tableReady = true;
     }).error(function() {
         console.log("error");
     });
-    
+
     $scope.submitOP = function() {
+        
+        $scope.ins = $scope.keepString($scope.ins);
         $scope.isSubmitting = true;
         var data = {
             "account": $scope.account,
@@ -100,7 +107,7 @@ function TambahUserCtrl($scope, $http, $state, $mdToast) {
             console.log(data);
         })
     };
-    
+
     $scope.submitInstansi = function() {
         $scope.isSubmitting = true;
         var data = {
@@ -119,13 +126,13 @@ function TambahUserCtrl($scope, $http, $state, $mdToast) {
                     .position('right')
                     .hideDelay(1000)
                     ).then(function(response) {
-                $state.go('home.tambahUser');
+                        $state.reload();
             });
         }).error(function(data) {
             console.log(data);
         })
     };
-    
+
     $scope.submitInstitusi = function() {
         $scope.isSubmitting = true;
         var data = {
@@ -151,7 +158,7 @@ function TambahUserCtrl($scope, $http, $state, $mdToast) {
             console.log(data);
         })
     };
-    
+
     $scope.submitKodeHal = function() {
         $scope.isSubmitting = true;
         var data = {
@@ -177,8 +184,10 @@ function TambahUserCtrl($scope, $http, $state, $mdToast) {
             console.log(data);
         })
     };
-    
+
     $scope.submitKodeUnit = function() {
+        $scope.inst = $scope.keepString($scope.inst);
+
         $scope.isSubmitting = true;
         var data = {
             "kode_unit": $scope.kodeunit,
