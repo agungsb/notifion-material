192f2eb43696ea4dc4da959abbe2271659b6cb9f
Agung Surya <agung.surya@gajahmerahterbang.com>
2015-10-10 20:40:39 +0700

    Updates

--- js/services/SuratKeluarService.js
+++ js/services/SuratKeluarService.js
@@ -26,16 +26,15 @@ app.service('SuratKeluarService', ['$http', '$rootScope', '$mdToast', 'Request',
                     "token": $rootScope.session_auth.token,
                     "pesan": pesan
                 };
-                alert(JSON.stringify(data));
-//                Request.putRequest("rejectSurat", data).success(function(feedback) {
-//                    console.log(feedback);
-//                    $mdToast.simple()
-//                            .content('Surat telah dikembalikan untuk dikoreksi')
-//                            .position('right')
-//                            .hideDelay(1000);
-//                }).error(function(data) {
-//                    console.log(data);
-//                });
+                Request.putRequest("koreksiSurat", data).success(function(feedback) {
+                    console.log(feedback);
+                    $mdToast.simple()
+                            .content('Surat telah dikembalikan untuk dikoreksi')
+                            .position('right')
+                            .hideDelay(1000);
+                }).error(function(data) {
+                    console.log(data);
+                });
             }
         };
     }]);
