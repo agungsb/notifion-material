43aaa6f31148800b02b83c5ee7333138b7efc529
Agung Surya <agung.surya@gajahmerahterbang.com>
2015-10-17 10:40:19 +0700

    Updates

--- js/services/Menu.js
+++ js/services/Menu.js
@@ -4,102 +4,102 @@ app.factory('menu', [
     '$location',
     '$rootScope',
     function($location, $rootScope) {
-
         var sections = [];
+        if ($rootScope.userInfoIsReady) {
+            sections.push({
+                name: 'Surat',
+                type: 'toggle',
+                pages: [{
+                        name: 'Surat Masuk',
+                        type: 'link',
+                        state: 'home.suratMasuk',
+                        allowed: true,
+                        id: 'suratmasuk',
+                        hascounter: true
+                    }, {
+                        name: 'Surat Keluar',
+                        state: 'home.suratKeluar',
+                        type: 'link',
+                        icon: 'fa fa-upload',
+                        allowed: true,
+                        id: 'suratkeluar',
+                        hascounter: true
+                    },
+                    {
+                        name: 'Surat Koreksi',
+                        state: 'home.suratKoreksi',
+                        type: 'link',
+                        icon: 'fa fa-edit',
+                        allowed: $rootScope.userInfo.jenis_user === '2',
+                        id: 'suratkoreksi',
+                        hascounter: true
+                    }],
+                allowed: true
+            });
 
-        sections.push({
-            name: 'Surat',
-            type: 'toggle',
-            pages: [{
-                    name: 'Surat Masuk',
-                    type: 'link',
-                    state: 'home.suratMasuk',
-                    allowed: true,
-                    id: 'suratmasuk',
-                    hascounter: true
-                }, {
-                    name: 'Surat Keluar',
-                    state: 'home.suratKeluar',
-                    type: 'link',
-                    icon: 'fa fa-upload',
-                    allowed: true,
-                    id: 'suratkeluar',
-                    hascounter: true
-                },
-                {
-                    name: 'Surat Koreksi',
-                    state: 'home.suratKoreksi',
-                    type: 'link',
-                    icon: 'fa fa-edit',
-                    allowed: $rootScope.userInfo.jenis_user === '2',
-                    id: 'suratkoreksi',
-                    hascounter: true
-                }],
-            allowed: true
-        });
+            sections.push({
+                name: 'Management User',
+                type: 'toggle',
+                pages: [{
+                        name: 'Tambah Account',
+                        type: 'link',
+                        state: 'home.tambahAccount',
+                        icon: 'fa fa-inbox',
+                        allowed: true
+                    }, {
+                        name: 'Tambah Instansi',
+                        state: 'home.tambahInstansi',
+                        type: 'link',
+                        icon: 'fa fa-upload',
+                        allowed: true
+                    },
+                    {
+                        name: 'Tambah Institusi',
+                        state: 'home.tambahInstitusi',
+                        type: 'link',
+                        icon: 'fa fa-edit',
+                        allowed: true
+                    }],
+//            allowed: $rootScope.userInfo.jenis_user === '2'
+                allowed: true
+            });
 
-        sections.push({
-            name: 'Management User',
-            type: 'toggle',
-            pages: [{
-                    name: 'Tambah Account',
-                    type: 'link',
-                    state: 'home.tambahAccount',
-                    icon: 'fa fa-inbox',
-                    allowed: true
-                }, {
-                    name: 'Tambah Instansi',
-                    state: 'home.tambahInstansi',
-                    type: 'link',
-                    icon: 'fa fa-upload',
-                    allowed: true
-                },
-                {
-                    name: 'Tambah Institusi',
-                    state: 'home.tambahInstitusi',
-                    type: 'link',
-                    icon: 'fa fa-edit',
-                    allowed: true
-                }],
+            sections.push({
+                name: 'Management Surat',
+                type: 'toggle',
+                pages: [{
+                        name: 'Tambah Kode Unit',
+                        state: 'home.tambahKodeUnit',
+                        type: 'link',
+                        icon: 'fa fa-upload',
+                        allowed: true
+                    }, {
+                        name: 'Tambah Kode Hal',
+                        state: 'home.tambahKodeHal',
+                        type: 'link',
+                        icon: 'fa fa-upload',
+                        allowed: true
+                    }],
 //            allowed: $rootScope.userInfo.jenis_user === '2'
-            allowed: true
-        });
-        
-        sections.push({
-            name: 'Management Surat',
-            type: 'toggle',
-            pages: [{
-                    name: 'Tambah Kode Unit',
-                    state: 'home.tambahKodeUnit',
-                    type: 'link',
-                    icon: 'fa fa-upload',
-                    allowed: true
-                },{
-                    name: 'Tambah Kode Hal',
-                    state: 'home.tambahKodeHal',
-                    type: 'link',
-                    icon: 'fa fa-upload',
-                    allowed: true
-                }],
-//            allowed: $rootScope.userInfo.jenis_user === '2'
-            allowed: true
-        });
+                allowed: true
+            });
 
-        sections.push({
-            name: 'Buat Surat',
-            state: 'home.buatSurat',
-            type: 'link',
-            allowed: true
+            sections.push({
+                name: 'Buat Surat',
+                state: 'home.buatSurat',
+                type: 'link',
+                allowed: true
 //            allowed: $rootScope.userInfo.jenis_user === '2'
-        });
-        
-        
-        sections.push({
-            name: 'Edit Biodata',
-            state: 'home.editBio',
-            type: 'link',
-            allowed: true
-        });
+            });
+
+
+            sections.push({
+                name: 'Edit Biodata',
+                state: 'home.editBio',
+                type: 'link',
+                allowed: true
+            });
+        }
 
         var self;
 
